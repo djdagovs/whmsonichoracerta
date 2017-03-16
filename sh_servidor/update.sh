0abe0df5014b697c0511e4fda1a2e2b5866b3f9e
Saulo Henrique <msaulohenrique@users.noreply.github.com>
2017-03-15 23:56:11 -0300

    Update update.sh

--- sh_servidor/update.sh
+++ sh_servidor/update.sh
@@ -6,10 +6,10 @@
 echo "Iniciando a atualizacao"
 cd /usr/local/cpanel/3rdparty/csonic/
 rm -f menu.php
-wget https://ninjashost.github.io/whmsonichoracerta/menu.txt
+wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/menu.txt
 mv -f menu.txt menu.php
 cd /usr/local/cpanel/3rdparty/csonic/tools
-wget https://ninjashost.github.io/whmsonichoracerta/tools/horacerta.txt
+wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/horacerta.txt
 mv -f horacerta.txt horacerta.php
 
 echo "Atualizacao concluida"
