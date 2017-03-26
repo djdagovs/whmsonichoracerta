6f3d0a9fd8f4cf735322f0e6b7281cdf7325fcd6
Saulo Henrique <msaulohenrique@users.noreply.github.com>
2017-03-26 15:09:58 -0300

    Update install-hc.sh

--- sh_servidor/install-hc.sh
+++ sh_servidor/install-hc.sh
@@ -20,6 +20,10 @@ wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/xmlapi.txt
 mv -f xmlapi.txt xmlapi.php
 wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/horacerta.txt
 mv -f horacerta.txt horacerta.php
+wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/programetes.txt
+mv -f programetes.txt programetes.php
+wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/player_topo.txt
+mv -f player_topo.txt player_topo.php
 wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/update.sh
 wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/remove.sh
 chmod +x update.sh
