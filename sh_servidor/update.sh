9263b0f7737dd32ab2b6670c52272252ed7b03e7
Saulo Henrique <msaulohenrique@users.noreply.github.com>
2017-03-26 15:27:35 -0300

    Update update.sh

--- sh_servidor/update.sh
+++ sh_servidor/update.sh
@@ -11,5 +11,9 @@ mv -f menu.txt menu.php
 cd /usr/local/cpanel/3rdparty/csonic/tools
 wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/horacerta.txt
 mv -f horacerta.txt horacerta.php
+wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/programetes.txt
+mv -f programetes.txt programetes.php
+wget https://ninjashost.github.io/whmsonichoracerta/sh_servidor/tools/player_topo.txt
+mv -f player_topo.txt player_topo.php
 
 echo "Atualizacao concluida"
